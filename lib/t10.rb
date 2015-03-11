a84e3deeeb5a33443eb90dbcb12d8d059c1a96de
mbrand12 <mbrand12.dump@gmail.com>
2015-03-11 17:32:45 +0100

    Add `Story` class, test and save_file
    - add `Story::new_adventure`, `Story::load_adventure`.
    - add `Story::save_adventure`, `Story::current_room`.
    - add `Story::ongoing_adventure?`.
    
    It also adds the save file in `data/game.yml`.
    
    Currently the class is implemented as a singleton (using only class
    methods) this might change in the future.

--- lib/t10.rb
+++ lib/t10.rb
@@ -1,3 +1,4 @@
 require 't10/room'
 require 't10/hero'
 require 't10/dungeon'
+require 't10/story'
---
+++ lib/t10/story.rb
@@ -0,0 +1,54 @@
+require 'yaml'
+require 't10/dungeon'
+
+module T10
+  class Story
+
+    @save_path = File.expand_path('../../../data/game.yml', __FILE__)
+    @save_path = nil
+    @dungeon = nil
+
+    def self.new_adventure
+
+      check_save_file
+      @dungeon = Dungeon.generate
+
+      data = {
+        dungeon: []
+      }
+      File.open(@save_path, 'w') {|f| YAML.dump(data, f)}
+    end
+
+    def self.save_adventure
+      check_save_file
+
+      data = {
+        dungeon: @dungeon
+      }
+      File.open(@save_path, 'w') {|f| YAML.dump(data, f)}
+    end
+
+    def self.load_adventure
+      check_save_file
+      data = YAML.load_file(@save_path)
+      @dungeon = data[:dungeon]
+    end
+
+    def self.current_room
+      @dungeon.find { |room| room.hero_here?}
+    end
+
+    def self.ongoing_adventure?
+      check_save_file
+      data = YAML.load_file(@save_path)
+      data.key?(:dungeon) && data[:dungeon].any? && data[:dungeon].size == 12
+    end
+
+    private
+
+    def self.check_save_file
+      fail RuntimeError, "Save file not found" unless File.exist?(@save_path)
+    end
+  end
+end
+
