3d70c971c3fffdb8da505b67b49d4d608c699356
mbrand12 <mbrand12.dump@gmail.com>
2015-12-09 22:16:56 +0100

    Add `cucumber` gem
    In this project I will use cucumber with minitest

--- Gemfile
+++ Gemfile
@@ -14,4 +14,5 @@ group :test do
   gem 'minitest',           '5.8.0'
   gem 'minitest-reporters', '1.1.5'
   gem 'simplecov',          '0.11.1', require: false
+  gem 'cucumber',           '2.1.0'
 end
---
+++ features/support/env.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+
+class MinitestWorld
+  extend Minitest::Assertions
+  attr_accessor :assertions
+
+  def initialize
+    self.assertions = 0
+  end
+end
+
+World do
+  MinitestWorld.new
+end
